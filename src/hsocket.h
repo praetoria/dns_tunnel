1774a5fa14feeb4bc0eb49f21918327dd280dec3
Hiski Ruhanen <hiski.ruhanen@f-secure.com>
2017-01-29 17:39:11 +0200

    added some comments

--- src/hsocket.h
+++ src/hsocket.h
@@ -9,6 +9,11 @@
 #include <netinet/in.h>
 #endif
 #include <string>
+/* When compiling for Windows, the first socket
+ * opened runs WSAStartup, and when the last of
+ * the currently active sockets closes, it runs
+ * WSACleanup.
+ */
 
 class hsocket {
     public:
@@ -19,15 +24,23 @@ class hsocket {
     hsocket& operator<<(const std::string&);
     hsocket& operator>>(std::string&);
     int good();
+    // addr has to be in network byte order
     void connect(uint16_t port,uint32_t addr);
     void connect(uint16_t port,const std::string& addr);
     void close();
+    // addr has to be in network byte order
     void bind(uint16_t port,uint32_t addr);
     void bind(uint16_t port,const std::string& addr);
     private:
+    // amount of open sockets
     static int socket_n;
     socket_t type;
     SOCKET s;
+    /* If using UDP the first to connect to a listening
+     * socket is marked as the only one to listen to.
+     * Kind of simulating a connection with UDP.
+     * send_addr contains the address to listen to.
+     */
     struct sockaddr_in send_addr;
     socklen_t send_len;
     struct sockaddr_in recv_addr;
